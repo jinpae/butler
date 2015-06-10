0816771685fd02780e9f013c081facc08a9c7b50
Jin Pae <jin@jinpae.com>
2015-06-10 20:54:57 +0900

    Style form related pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def pluralize_without_count(count, singular, plural = nil)
 		((count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize))
 	end
+
+	def home_page?
+		@result ||= (controller_name == 'home') && (action_name == 'index')
+	end
 end
