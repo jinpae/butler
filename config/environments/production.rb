0a9328c6120fd304d29a5c806ac771f26ee5988a
Jin Pae <jin@jinpae.com>
2015-06-20 14:17:12 +0900

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
 	config.action_mailer.smtp_settings = {
 		port: '587',
 		address: 'smtp.mandrillapp.com',
-		user_name: ENV['MANDRILL_USERNAME']
-		password: ENV['MANDRILL_APIKEY']
+		user_name: ENV['MANDRILL_USERNAME'],
+		password: ENV['MANDRILL_APIKEY'],
 		domain: 'heroku.com',
 		authentication: :plain,
 		enable_starttls_auto: true
