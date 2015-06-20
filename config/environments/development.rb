34872365f68e7324cb375ce81f36bd2da82bfda3
Jin Pae <jin@jinpae.com>
2015-06-20 14:14:40 +0900

    Update settings to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -45,7 +45,7 @@ Rails.application.configure do
 		port: 25,
 		address: 'smtp.mandrillapp.com',
 		user_name: Rails.application.secrets.mandrill_username,
-		password: Rails.application.secrets.mandrill_password,
+		password: Rails.application.secrets.mandrill_apikey,
 		domain: 'localhost',
 		authentication: :login,
 		enable_starttls_auto: true
